209d44abe860c72c08e1a3621655766e6b01ed62
zhusong <1017485755@qq.com>
2021-05-21 02:32:34 +0800

    version2

--- GitTest/src/bigdata/itcast/cn/HelloPrint.java
+++ GitTest/src/bigdata/itcast/cn/HelloPrint.java
@@ -3,5 +3,6 @@ package bigdata.itcast.cn;
 public class HelloPrint {
     public static void main(String[] args) {
         System.out.println("this is version1,I love China");
+        System.out.println("this is version2,I love ITcast");
     }
 }
